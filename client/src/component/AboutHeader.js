0e6074fb9d9efd73122cce4af4c7b706a2372a16
Caroline Strickland <carolinelstrickland@outlook.com>
2021-02-08 19:13:47 -0600

    landing redesign

--- client/src/component/AboutHeader.js
+++ client/src/component/AboutHeader.js
@@ -8,10 +8,10 @@ class AboutHeader extends Component {
     render() {
         return (
             <React.Fragment>
-                <div style={{ backgroundColor: "#acffe4", position: "fixed", left: "0", bottom: "0", right: "0" }}>
+                <div style={{ backgroundColor: "#666699", position: "fixed", left: "0", bottom: "0", right: "0" }}>
                     {/* <a href="https://www.instagram.com/brows.by.alice/?igshid=gz8d6g70lsza" target="_blank"><FaInstagram style={{ height: "40px", width: "40px", color: "white" }} /> </a>
                     <a href="https://www.google.com/maps/place/Brows+by+Alice/@42.125594,-87.8361676,13z/data=!4m5!3m4!1s0x0:0xdf5e400ffa6f6705!8m2!3d42.125594!4d-87.8361676" target="_blank"><FaMapMarkerAlt style={{ height: "40px", width: "40px", color: "white" }} /> </a> */}
-                    <p style={{color: "black" , fontSize: "15px", fontFamily: 'Playfair Display', textAlign:"center"}}>
+                    <p style={{color: "#c2c2e6" , fontSize: "15px", fontFamily: 'Playfair Display', textAlign:"center"}}>
                         Copyright BrowsByAlice®
                     </p>
                 </div>
